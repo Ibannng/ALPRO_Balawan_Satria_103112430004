028f67b0c5af74859245ecc65c20a24bb1453bdb
Ibannng <130421877+Ibannng@users.noreply.github.com>
2024-10-02 19:34:08 +0700

    Add files via upload

---
+++ 03_I0_Tipe_Data_dan_Variable/TP/kpersegi.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    sisi := 27
+    keliling := 4 * sisi
+    luas := sisi * sisi
+
+    fmt.Println("Alun-alun dengan panjang sisi", sisi, "meter:")
+    fmt.Println("Keliling:", keliling, "meter")
+    fmt.Println("Luas:", luas, "meter persegi")
+}
